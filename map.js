5c7e0244aa8f5aa4b5da889c0c7bd76c24b6bf0b
mertsuru <msuerue@stud.hs-heilbronn.de>
2017-04-21 20:01:27 +0200

    google maps standort sind responisve

--- map.js
+++ map.js
@@ -1,120 +1,175 @@
+//
+// var myLatlng = new google.maps.LatLng(49.12259, 9.210834999999975);
+// var marker;
+// var map;
+// window.addEventListener("resize", rememberCenter);
+// //HHN Sontheim
+// function initMap() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 17,
+//         center: {lat: 49.12259, lng: 9.210834999999975}
+//     });
+//
+//     marker.addListener('click', toggleBounce);
+//
+//     google.maps.event.addDomListener(window, 'load', initialize);
+//     google.maps.event.addDomListener(window, "resize", function () {
+//         var center = map.getCenter();
+//         google.maps.event.trigger(map, "resize");
+//         map.setCenter(center);
+//     });
+// }
+// //HHN Sontheim
+// function hhnsontheim() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 17,
+//         center: {lat: 49.12259, lng: 9.210834999999975}
+//
+//     });
+// //Marker fällt auf Ziel!
+//     marker = new google.maps.Marker({
+//         map: map,
+//         draggable: true,
+//         animation: google.maps.Animation.DROP,
+//         position: {lat: 49.12259, lng: 9.210834999999975}
+//     });
+//     marker.addListener('click', toggleBounce);
+// }
+// // damit der Marker springt
+// function toggleBounce() {
+//     if (marker.getAnimation() !== null) {
+//         marker.setAnimation(null);
+//     } else {
+//         marker.setAnimation(google.maps.Animation.BOUNCE);
+//     }
+// }
+// //HHN Europacampus
+// function campus() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 17,
+//         center: {lat: 49.148306, lng: 9.21648}
+//     });
+// //Marker fällt auf Ziel!
+//     marker = new google.maps.Marker({
+//         map: map,
+//         draggable: true,
+//         animation: google.maps.Animation.DROP,
+//         position: {lat: 49.148306, lng: 9.21648}
+//     });
+//     marker.addListener('click', toggleBounce);
+// }
+// // damit der Marker springt
+// function toggleBounce() {
+//     if (marker.getAnimation() !== null) {
+//         marker.setAnimation(null);
+//     } else {
+//         marker.setAnimation(google.maps.Animation.BOUNCE);
+//     }
+// }
+// //HHN Schwäbisch Hall
+// function hhnsha() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 18,
+//         center: {lat: 49.112536, lng: 9.743618}
+//     });
+// //Marker fällt auf Ziel!
+//     marker = new google.maps.Marker({
+//         map: map,
+//         draggable: true,
+//         animation: google.maps.Animation.DROP,
+//         position: {lat: 49.112536, lng: 9.743618}
+//     });
+//     marker.addListener('click', toggleBounce);
+// }
+// // damit der Marker springt
+// function toggleBounce() {
+//     if (marker.getAnimation() !== null) {
+//         marker.setAnimation(null);
+//     } else {
+//         marker.setAnimation(google.maps.Animation.BOUNCE);
+//     }
+// }
+// //HHN Künzelsau
+// function hhnkun() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 17,
+//         center: {lat: 49.275552, lng: 9.712164}
+//     });
+// //Marker fällt auf Ziel!
+//     marker = new google.maps.Marker({
+//         map: map,
+//         draggable: true,
+//         animation: google.maps.Animation.DROP,
+//         position: {lat: 49.275552, lng: 9.712164}
+//     });
+//     marker.addListener('click', toggleBounce);
+// }
+// // damit der Marker springt
+// function toggleBounce() {
+//     if (marker.getAnimation() !== null) {
+//         marker.setAnimation(null);
+//     } else {
+//         marker.setAnimation(google.maps.Animation.BOUNCE);
+//     }
+// }
 
-var myLatlng = new google.maps.LatLng(49.12259, 9.210834999999975);
-var marker;
+
 var map;
-window.addEventListener("resize", rememberCenter);
-//HHN Sontheim
+var marker;
+
+$(function () {
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
+
 function initMap() {
-     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: {lat: 49.12259, lng: 9.210834999999975}
-    });
+// default: Heilbronn center
+    var location = new google.maps.LatLng(49.142598, 9.218245);
 
-    marker.addListener('click', toggleBounce);
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-    google.maps.event.addDomListener(window, "resize", function () {
-        var center = map.getCenter();
-        google.maps.event.trigger(map, "resize");
-        map.setCenter(center);
-    });
-}
-//HHN Sontheim
-function hhnsontheim() {
-     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: {lat: 49.12259, lng: 9.210834999999975}
-
-    });
-//Marker fällt auf Ziel!
-    marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.DROP,
-        position: {lat: 49.12259, lng: 9.210834999999975}
-    });
-    marker.addListener('click', toggleBounce);
-}
-// damit der Marker springt
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
-//HHN Europacampus
-function campus() {
-     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: {lat: 49.148306, lng: 9.21648}
-    });
-//Marker fällt auf Ziel!
-    marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.DROP,
-        position: {lat: 49.148306, lng: 9.21648}
-    });
-    marker.addListener('click', toggleBounce);
-}
-// damit der Marker springt
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
-//HHN Schwäbisch Hall
-function hhnsha() {
-     map = new google.maps.Map(document.getElementById('map'), {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: location,
         zoom: 18,
-        center: {lat: 49.112536, lng: 9.743618}
-    });
-//Marker fällt auf Ziel!
+        panControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+    map = new google.maps.Map(mapCanvas, mapOptions);
+
     marker = new google.maps.Marker({
         map: map,
         draggable: true,
         animation: google.maps.Animation.DROP,
-        position: {lat: 49.112536, lng: 9.743618}
+        position: location
     });
-    marker.addListener('click', toggleBounce);
+
+    google.maps.event.addDomListener(window, "resize", recenterMap);
 }
-// damit der Marker springt
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
+
+function recenterMap() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.panTo(center);
 }
-//HHN Künzelsau
+
+function hhnsontheim() {
+    var location = new google.maps.LatLng(49.12259, 9.210834999999975);
+    marker.setPosition(location);
+    map.panTo(location);
+};
+
 function hhnkun() {
-     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: {lat: 49.275552, lng: 9.712164}
-    });
-//Marker fällt auf Ziel!
-    marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.DROP,
-        position: {lat: 49.275552, lng: 9.712164}
-    });
-    marker.addListener('click', toggleBounce);
-}
-// damit der Marker springt
-function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
-function rememberCenter() {
-    var currCenter = map.getCenter();
-    console.log('currCenter');
-}
+    var location = new google.maps.LatLng(49.275552, 9.712164);
+    marker.setPosition(location);
+    map.panTo(location);
+};
 
+function hhnsha() {
+    var location = new google.maps.LatLng(49.112536, 9.743618);
+    marker.setPosition(location);
+    map.panTo(location);
+};
 
+function campus() {
+    var location = new google.maps.LatLng(49.148306, 9.21648);
+    marker.setPosition(location);
+    map.panTo(location);
+};

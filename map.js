09964c8d89fe62d718d8141aed240eddaff16dae
mertsuru <msuerue@stud.hs-heilbronn.de>
2017-04-23 15:25:39 +0200

    marker dropen beim anklicken der links + dropdown leiste funktioniert

--- map.js
+++ map.js
@@ -1,121 +1,6 @@
-//
-// var myLatlng = new google.maps.LatLng(49.12259, 9.210834999999975);
-// var marker;
-// var map;
-// window.addEventListener("resize", rememberCenter);
-// //HHN Sontheim
-// function initMap() {
-//      map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 17,
-//         center: {lat: 49.12259, lng: 9.210834999999975}
-//     });
-//
-//     marker.addListener('click', toggleBounce);
-//
-//     google.maps.event.addDomListener(window, 'load', initialize);
-//     google.maps.event.addDomListener(window, "resize", function () {
-//         var center = map.getCenter();
-//         google.maps.event.trigger(map, "resize");
-//         map.setCenter(center);
-//     });
-// }
-// //HHN Sontheim
-// function hhnsontheim() {
-//      map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 17,
-//         center: {lat: 49.12259, lng: 9.210834999999975}
-//
-//     });
-// //Marker fällt auf Ziel!
-//     marker = new google.maps.Marker({
-//         map: map,
-//         draggable: true,
-//         animation: google.maps.Animation.DROP,
-//         position: {lat: 49.12259, lng: 9.210834999999975}
-//     });
-//     marker.addListener('click', toggleBounce);
-// }
-// // damit der Marker springt
-// function toggleBounce() {
-//     if (marker.getAnimation() !== null) {
-//         marker.setAnimation(null);
-//     } else {
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-// }
-// //HHN Europacampus
-// function campus() {
-//      map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 17,
-//         center: {lat: 49.148306, lng: 9.21648}
-//     });
-// //Marker fällt auf Ziel!
-//     marker = new google.maps.Marker({
-//         map: map,
-//         draggable: true,
-//         animation: google.maps.Animation.DROP,
-//         position: {lat: 49.148306, lng: 9.21648}
-//     });
-//     marker.addListener('click', toggleBounce);
-// }
-// // damit der Marker springt
-// function toggleBounce() {
-//     if (marker.getAnimation() !== null) {
-//         marker.setAnimation(null);
-//     } else {
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-// }
-// //HHN Schwäbisch Hall
-// function hhnsha() {
-//      map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 18,
-//         center: {lat: 49.112536, lng: 9.743618}
-//     });
-// //Marker fällt auf Ziel!
-//     marker = new google.maps.Marker({
-//         map: map,
-//         draggable: true,
-//         animation: google.maps.Animation.DROP,
-//         position: {lat: 49.112536, lng: 9.743618}
-//     });
-//     marker.addListener('click', toggleBounce);
-// }
-// // damit der Marker springt
-// function toggleBounce() {
-//     if (marker.getAnimation() !== null) {
-//         marker.setAnimation(null);
-//     } else {
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-// }
-// //HHN Künzelsau
-// function hhnkun() {
-//      map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 17,
-//         center: {lat: 49.275552, lng: 9.712164}
-//     });
-// //Marker fällt auf Ziel!
-//     marker = new google.maps.Marker({
-//         map: map,
-//         draggable: true,
-//         animation: google.maps.Animation.DROP,
-//         position: {lat: 49.275552, lng: 9.712164}
-//     });
-//     marker.addListener('click', toggleBounce);
-// }
-// // damit der Marker springt
-// function toggleBounce() {
-//     if (marker.getAnimation() !== null) {
-//         marker.setAnimation(null);
-//     } else {
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-// }
-
-
 var map;
 var marker;
+
 
 $(function () {
     google.maps.event.addDomListener(window, 'load', initMap);
@@ -128,10 +13,10 @@ function initMap() {
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
         center: location,
-        zoom: 18,
+        zoom: 17,
         panControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-}
+    }
     map = new google.maps.Map(mapCanvas, mapOptions);
 
     marker = new google.maps.Marker({
@@ -150,26 +35,67 @@ function recenterMap() {
     map.panTo(center);
 }
 
+//Standort Sontheim
 function hhnsontheim() {
     var location = new google.maps.LatLng(49.12259, 9.210834999999975);
     marker.setPosition(location);
     map.panTo(location);
+    marker.setAnimation(google.maps.Animation.DROP);
+    //Auskommentiert da sich der Standort verschiebt beim klicken
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     var coordInfoWindow = new google.maps.InfoWindow();
+    //     coordInfoWindow.setContent('Campus Heilbronn - Sontheim');
+    //     coordInfoWindow.setPosition(location);
+    //     coordInfoWindow.open(map);
+    // });
+
 };
 
+//Standort Künzelsau
 function hhnkun() {
     var location = new google.maps.LatLng(49.275552, 9.712164);
     marker.setPosition(location);
     map.panTo(location);
+    marker.setAnimation(google.maps.Animation.DROP);
+    //Auskommentiert da sich der Standort verschiebt beim klicken
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     var coordInfoWindow = new google.maps.InfoWindow();
+    //     coordInfoWindow.setContent('Canpus Künzelsau - Reinhold-Würth-Hochschule');
+    //     coordInfoWindow.setPosition(location);
+    //     coordInfoWindow.open(map);
+    // });
+
 };
 
+
+//Standort Schäbisch Hall
 function hhnsha() {
     var location = new google.maps.LatLng(49.112536, 9.743618);
     marker.setPosition(location);
     map.panTo(location);
+    marker.setAnimation(google.maps.Animation.DROP);
+    //Auskommentiert da sich der Standort verschiebt beim klicken
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     var coordInfoWindow = new google.maps.InfoWindow();
+    //     coordInfoWindow.setContent('Campus Schwäbisch Hall');
+    //     coordInfoWindow.setPosition(location);
+    //     coordInfoWindow.open(map);
+    // });
+
 };
 
+//Standort Europaplatz
 function campus() {
     var location = new google.maps.LatLng(49.148306, 9.21648);
     marker.setPosition(location);
     map.panTo(location);
+    marker.setAnimation(google.maps.Animation.DROP);
+    //Auskommentiert da sich der Standort verschiebt beim klicken
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     var coordInfoWindow = new google.maps.InfoWindow();
+    //     coordInfoWindow.setContent('Campus Heilbronn - Am Europaplatz');
+    //     coordInfoWindow.setPosition(location);
+    //     coordInfoWindow.open(map);
+    // });
+
 };
